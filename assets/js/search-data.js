a295edaf5e50591e1e8282073281de745be1ab74
sayanvinod <sayanvinod@gmail.com>
2025-06-24 17:51:39 +0000

    Deploying to gh-pages from @ sayanvinod/sayanvinod.github.io@79af1a7a9bdddd00f8e8bf75f0ff3b09654374f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "nav-people",
           title: "people",
